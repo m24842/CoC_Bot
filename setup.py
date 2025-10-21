c3d31ea74c5d04ca5b6a1ad596b01cd9b29207b7
m24842 <138349218+m24842@users.noreply.github.com>
2025-10-21 12:35:49 -0400

    save

--- setup.py
+++ setup.py
@@ -33,18 +33,19 @@ else:
     print(f"src/configs.py already exists, skipping creation.")
 
 if web_app_setup:
-    print("Setting up web app systemd service...")
-    user = os.environ['USER']
-    with open("app/coc_bot_web_app.template.service", "r") as f: content = f.read()
-    content = content.replace("<user>", user)
-    with open("app/coc_bot_web_app.service", "w") as f: f.write(content)
-    
-    os.system("sudo cp app/coc_bot_web_app.service /etc/systemd/system/coc_bot_web_app.service")
-    os.system("sudo systemctl daemon-reload")
-    os.system("sudo systemctl enable coc_bot_web_app.service")
-    
-    start_web_app = input("Start web app systemd service? (y/n): ").lower() == 'y'
-    if start_web_app:
-        os.system("sudo systemctl start coc_bot_web_app")
-    else:
-        print("Use 'sudo systemctl start coc_bot_web_app' to start web app service.")
+    web_app_service = input("Setup web app systemd service? (y/n): ").lower() == 'y'
+    if web_app_service:
+        user = os.environ['USER']
+        with open("app/coc_bot_web_app.template.service", "r") as f: content = f.read()
+        content = content.replace("<user>", user)
+        with open("app/coc_bot_web_app.service", "w") as f: f.write(content)
+        
+        os.system("sudo cp app/coc_bot_web_app.service /etc/systemd/system/coc_bot_web_app.service")
+        os.system("sudo systemctl daemon-reload")
+        os.system("sudo systemctl enable coc_bot_web_app.service")
+        
+        start_web_app = input("Start web app systemd service? (y/n): ").lower() == 'y'
+        if start_web_app:
+            os.system("sudo systemctl start coc_bot_web_app")
+        else:
+            print("Use 'sudo systemctl start coc_bot_web_app' to start web app service.")
