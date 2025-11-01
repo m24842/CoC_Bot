a5871d980ee29d1078284aca6e92cc87e8244aea
m24842 <138349218+m24842@users.noreply.github.com>
2025-11-01 14:08:37 -0400

    save

--- src/configs.template.py
+++ src/configs.template.py
@@ -1,7 +1,6 @@
 ######################
 # == User Configs == #
 ######################
-DEBUG = False
 
 # OPTIONAL: Telegram notifications (enter empty strings to disable)
 TELEGRAM_BOT_TOKEN = "" # (e.g. 123456789:ABCdefGHIjkl-MNO_pqrSTUvwxYZ)
@@ -11,7 +10,6 @@ WEB_APP_URL = "" # (e.g. 12.34.567.890:1234)
 WEB_APP_PORT = 1234
 
 # REQUIRED: General Settings
-DISABLE_DEEVICE_SLEEP = True
 CHECK_INTERVAL = 5 * 60 # seconds
 
 # REQUIRED: Upgrade settings
@@ -28,8 +26,10 @@ EXCLUDE_CLAN_TROOPS = True
 ########################
 # == System Configs == #
 ########################
-ADB_ADDRESS = "127.0.0.1:5555"
+DEBUG = False
+DISABLE_DEEVICE_SLEEP = True
 WINDOW_DIMS = (1920, 1080) # width, height
+ADB_ADDRESS = "127.0.0.1:5555"
 UPGRADER_ASSETS_DIR = "assets/upgrader"
 ATTACKER_ASSETS_DIR = "assets/attacker"
 NOTIFICATIONS_DB_PATH = "notifications.db"
