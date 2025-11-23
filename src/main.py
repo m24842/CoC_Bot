1651789a652b566fab697fc0f67bee6be9c3c47a
m24842 <138349218+m24842@users.noreply.github.com>
2025-11-23 16:50:07 -0500

    save

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
 from log import enable_logging
 from coc_bot import CoC_Bot
 
-enable_logging()
-
 if __name__ == "__main__":
+    enable_logging()
     bot = CoC_Bot()
     print("\033c", end="")
     bot.run()
--- src/start.sh
+++ src/start.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-SESSION_NAME="CoC_Bot"
+read -p "Enter instance ID: " INSTANCE_ID
+
+BASE_NAME="CoC_Bot"
+SESSION_NAME="${BASE_NAME}_${INSTANCE_ID}"
 
 if tmux has-session -t $SESSION_NAME 2>/dev/null; then
     echo "$SESSION_NAME session already exists. Attaching..."
     tmux attach -t $SESSION_NAME
 else
     echo "Starting new tmux session: $SESSION_NAME"
-    tmux new-session -d -s $SESSION_NAME "sudo /Users/madison/CoC_Bot/.venv/bin/python /Users/madison/CoC_Bot/src/main.py"
+    tmux new-session -d -s $SESSION_NAME "sudo /Users/madison/CoC_Bot/.venv/bin/python /Users/madison/CoC_Bot/src/main.py --id $INSTANCE_ID"
     echo "$SESSION_NAME session started and detached."
 fi
