56fff671448d6e2a6c2f6f29c448c929d6d0358c
m24842 <138349218+m24842@users.noreply.github.com>
2025-11-24 13:35:55 -0500

    save

---
+++ app/wsgi.py
@@ -0,0 +1,17 @@
+import sys
+import os
+
+project_home = '/home/<username>/CoC_Bot'
+app_dir = os.path.join(project_home, 'app')
+src_dir = os.path.join(project_home, 'src')
+
+for path in [project_home, app_dir, src_dir]:
+    if path not in sys.path:
+        sys.path.insert(0, path)
+
+os.environ['FLASK_APP'] = 'app/app.py'
+
+from app import get_known_instances, init_scheduler
+get_known_instances()
+init_scheduler()
+from app import app as application
