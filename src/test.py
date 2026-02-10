38cdd808cc40d0e5cc2a80fb1f2d34395f691d43
m24842 <138349218+m24842@users.noreply.github.com>
2026-02-10 10:53:09 -0500

    save

--- src/test.py
+++ src/test.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     bot = CoC_Bot()
     # Frame_Handler.screenshot()
     # start_coc()
-    bot.run()
+    # bot.run()
     # to_home_base()
     # bot.upgrader.run_home_base()
     # bot.upgrader.home_upgrade()
