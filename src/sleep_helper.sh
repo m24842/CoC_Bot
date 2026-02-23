bbf8788ac76a19f5e15027d9c6443906c554b4ea
m24842 <138349218+m24842@users.noreply.github.com>
2026-02-23 17:25:00 -0500

    save

--- src/sleep_helper.sh
+++ src/sleep_helper.sh
@@ -6,12 +6,17 @@ set_sleep() {
     pmset -a sleep "$1"
 }
 
+set_disablesleep() {
+    pmset -a disablesleep "$1"
+}
+
 parent_alive() {
     kill -0 "$1" 2>/dev/null
 }
 
 cleanup() {
     set_sleep "$ORIGINAL_SLEEP"
+    set_disablesleep 0
     exit 0
 }
 
@@ -20,6 +25,7 @@ trap cleanup SIGTERM SIGINT
 ORIGINAL_SLEEP=$(pmset -g | grep ' sleep ' | awk '{print $2}')
 
 set_sleep 0
+set_disablesleep 1
 
 if kill -0 "$PARENT_PID" 2>/dev/null; then
     wait "$PARENT_PID"
