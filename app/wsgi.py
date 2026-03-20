50f47000197c8760f0f5a04c5d6be6fde963d653
m24842 <138349218+m24842@users.noreply.github.com>
2026-03-20 15:18:08 -0400

    save

---
+++ app/wsgi.py
@@ -0,0 +1,15 @@
+import sys
+import os
+
+PYTHONANYWHERE_USERNAME = ""
+
+project_home = f'/home/{PYTHONANYWHERE_USERNAME}/CoC_Bot'
+app_dir = os.path.join(project_home, 'app')
+
+for path in [project_home, app_dir]:
+    if path not in sys.path:
+        sys.path.insert(0, path)
+
+os.environ['FLASK_APP'] = 'app/app.py'
+
+from app import app as application
