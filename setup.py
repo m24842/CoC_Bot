f18327d2cbfc775a35f82d02b2910791a22335c7
m24842 <138349218+m24842@users.noreply.github.com>
2026-03-25 18:53:15 -0400

    save

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import os
-import sys
 import shutil
 from pathlib import Path
 
+py311 = shutil.which("python3.11")
 dir_path = Path(__file__).parent.resolve()
 
 bot_packages = dir_path / "src" / "requirements.txt"
@@ -12,12 +12,12 @@ web_app_packages = dir_path / "app" / "requirements.txt"
 bot_setup = input("Install bot dependencies? (y/n): ").lower() == 'y'
 web_app_setup = input("Install web app dependencies? (y/n): ").lower() == 'y'
 
-if not Path.exists(dir_path / ".venv"): os.system(f"{sys.executable} -m venv {dir_path / '.venv'}")
+if not Path.exists(dir_path / ".venv"): os.system(f"{py311} -m venv {dir_path / '.venv'}")
 
 if bot_setup:
-    os.system(f"{sys.executable} -m pip install -r {bot_packages}")
+    os.system(f"{py311} -m pip install -r {bot_packages}")
 if web_app_setup:
-    os.system(f"{sys.executable} -m pip install -r {web_app_packages}")
+    os.system(f"{py311} -m pip install -r {web_app_packages}")
 
 if not Path.exists(dir_path / "src" / "configs.py"):
     shutil.copy(dir_path / "src" / "configs.template.py", dir_path / "src" / "configs.py")
