4598e6cedb1503765f0d5b880d85cb95401f1f02
m24842 <138349218+m24842@users.noreply.github.com>
2026-05-26 11:15:29 -0400

    save

--- src/log.py
+++ src/log.py
@@ -6,6 +6,18 @@ from pathlib import Path
 loguru.logger.remove()
 warnings.filterwarnings("ignore", category=UserWarning, module='torch')
 
+class Logger:
+    def __init__(self, level):
+        self.level = level
+
+    def write(self, data):
+        data = data.strip()
+        if data:
+            loguru.logger.log(self.level, data)
+
+    def flush(self):
+        pass
+
 class Tee:
     def __init__(self, *streams):
         self.streams = streams
@@ -15,15 +27,15 @@ class Tee:
             try:
                 s.write(data)
                 s.flush()
-            except (KeyboardInterrupt, SystemExit): raise
-            except: pass
+            except:
+                pass
 
     def flush(self):
         for s in self.streams:
             try:
                 s.flush()
-            except (KeyboardInterrupt, SystemExit): raise
-            except: pass
+            except:
+                pass
 
 def enable_logging(id):
     if getattr(sys, "frozen", False):
@@ -32,16 +44,28 @@ def enable_logging(id):
         LOG_DIR = APP_DATA_DIR / "debug"
     else:
         LOG_DIR = Path("debug")
+
     LOG_DIR.mkdir(exist_ok=True)
 
     LOG_PATH = LOG_DIR / f"{id}.log"
-    log_file = open(LOG_PATH, "a", buffering=1)
-    try: LOG_PATH.chmod(0o666)
-    except: pass
+
+    exclude_modules = [
+        "pyminitouch",
+    ]
+
+    loguru.logger.add(
+        LOG_PATH,
+        rotation="10 MB",
+        retention=5,
+        compression="zip",
+        enqueue=True,
+        format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}",
+        filter=lambda record: not any(record["name"].startswith(mod) for mod in exclude_modules),
+    )
 
     if getattr(sys, "frozen", False):
-        sys.stdout = Tee(*[log_file])
-        sys.stderr = Tee(*[log_file])
+        sys.stdout = Logger("INFO")
+        sys.stderr = Logger("ERROR")
     else:
-        sys.stdout = Tee(*[log_file, sys.__stdout__])
-        sys.stderr = Tee(*[log_file, sys.__stderr__])
+        sys.stdout = Tee(sys.__stdout__, Logger("INFO"))
+        sys.stderr = Tee(sys.__stderr__, Logger("ERROR"))
