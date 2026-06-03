deeef07f978cad540bd9e01cb3a9e453eeba1d69
m24842 <138349218+m24842@users.noreply.github.com>
2026-06-03 16:47:06 -0400

    save

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,11 +6,20 @@ source .venv/bin/activate
 rm -rf build dist *.spec
 
 pyinstaller \
-  --name "CoC Bot" \
-  --windowed \
-  --icon="media/CoC_Bot.icns" \
-  --add-data "assets:assets" \
-  --add-data "src/gui_server:gui_server" \
-  --add-data "src/sleep_helper.sh:." \
-   --add-data "src/uiautomator2:uiautomator2" \
-  "src/main.py"
+    --name "CoC Bot" \
+    --windowed \
+    --icon="media/CoC_Bot.icns" \
+    --add-data "assets:assets" \
+    --add-data "src/gui_server:gui_server" \
+    --add-data "src/sleep_helper.sh:." \
+     --add-data "src/uiautomator2:uiautomator2" \
+    "src/main.py"
+
+if [[ "$(uname -s)" == "Darwin" ]]; then
+    rm -rf "/Applications/CoC Bot.app"
+    cp -R "dist/CoC Bot.app" "/Applications/"
+
+    echo "Installed at:"
+    echo "/Applications/CoC Bot.app"
+    open "/Applications/CoC Bot.app"
+fi
