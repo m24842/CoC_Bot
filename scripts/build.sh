561b3a704406551b876ccc18d201790042621da0
m24842 <138349218+m24842@users.noreply.github.com>
2026-06-09 19:44:23 -0400

    save

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,11 +8,11 @@ rm -rf build dist *.spec
 pyinstaller \
     --name "CoC Bot" \
     --windowed \
-    --icon="media/CoC_Bot.icns" \
+    --icon "media/CoC_Bot.icns" \
     --add-data "assets:assets" \
     --add-data "src/gui_server:gui_server" \
     --add-data "src/sleep_helper.sh:." \
-     --add-data "src/uiautomator2:uiautomator2" \
+    --additional-hooks-dir=hooks \
     "src/main.py"
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
